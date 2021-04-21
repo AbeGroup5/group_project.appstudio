a3af6a1b50e08ca6cc33fb8fdb8371f38bbc47f7
Dpfeifer2000 <34215627+Dpfeifer2000@users.noreply.github.com>
2021-04-21 11:51:48 -0500

    update 11:51 4-21-21
    rj and google sign in partial

---
+++ forms/HomeSearch/HomeSearch.js
@@ -0,0 +1,23 @@
+Hamburger1.onclick=function(s){
+if (typeof(s) == "object") 
+    return
+switch(s) {
+case "Login":
+    ChangeForm(Login)
+    break
+case "Post":
+    ChangeForm(MakePost)
+    break
+}
+}
+
+let univ = drpUniversity.value
+let dep = drpDepartment.value
+let classNum = iptClassNumber.value
+let prof = iptLastName.value
+
+
+btnSubmit.onclick=function(){
+  //connect to sql database with parameters
+  //take us to post page, nit yet created
+}
